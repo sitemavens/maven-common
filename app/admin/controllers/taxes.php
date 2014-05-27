71d61e83284f869bd0f85148e8c384cfb3089fe6
Lucas Perez <lucas@dinkuminteractive.com>
2014-05-27 09:50:25 -0300

    Rename taxes endpoint from common to maven

--- app/admin/controllers/taxes.php
+++ app/admin/controllers/taxes.php
@@ -14,11 +14,11 @@ class Taxes extends \Maven\Admin\Controllers\MavenAdminController {
 	
 	public function registerRoutes( $routes ){
 		
-		$routes[ '/common/taxes' ] = array(
+		$routes[ '/maven/taxes' ] = array(
 			array( array( $this, 'getTaxes' ), \WP_JSON_Server::READABLE ),
 			array( array( $this, 'newTax' ), \WP_JSON_Server::CREATABLE | \WP_JSON_Server::ACCEPT_JSON ),
 		);
-		$routes[ '/common/taxes/(?P<id>\d+)' ] = array(
+		$routes[ '/maven/taxes/(?P<id>\d+)' ] = array(
 			array( array( $this, 'getTax' ), \WP_JSON_Server::READABLE ),
 			array( array( $this, 'editTax' ), \WP_JSON_Server::EDITABLE | \WP_JSON_Server::ACCEPT_JSON ),
 			array( array( $this, 'deleteTax' ), \WP_JSON_Server::DELETABLE ),
