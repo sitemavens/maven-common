d1d31860417985df17785f639b8a0a187b2713e4
kamalcha <mail@kamalchaneman.com>
2014-06-11 23:00:35 +0700

    settings page
    Signed-off-by: kamalcha <mail@kamalchaneman.com>

--- app/views/admin/admin.php
+++ app/views/admin/admin.php
@@ -1,4 +1,4 @@
-<div ng-app="mavenApp">
+<div ng-app="mavenApp" class="wrap">
 
 	<div class="header" ng-controller='MainNavCtrl'>
 		<ul class="nav nav-pills pull-right" >
@@ -8,10 +8,10 @@
 			<li class="" ng-class="{active:isActive('/taxes')}"><a ng-href="#/taxes"><i class="glyphicon glyphicon-flag"></i>&nbsp;Taxes</a></li>
 			<li class="" ng-class="{active:isActive('/settings')}"><a ng-href="#/settings"><i class="glyphicon glyphicon-cog"></i>&nbsp;Settings</a></li>
 		</ul>
-		<h1 class="text-muted">Maven</h1>
+		<h1 class="text-muted"><img src="http://www.sitemavens.com/wp-content/uploads/2014/05/SM-logo-transparent_rev.png" alt="SiteMavens"></h1>
 	</div>
 
 	<!-- Add your site or application content here -->
 	<div data-loading >Loading...</div>
-	<div class="container" ng-view></div>
+	<div ng-view></div>
 </div>
