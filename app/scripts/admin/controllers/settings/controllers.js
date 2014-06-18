b5f38bc006d736da73881cac65659ee4ad81835e
mustela <ejankowski@gmail.com>
2014-06-18 14:23:41 -0300

    Settings files added

---
+++ app/scripts/admin/controllers/settings/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var app = angular.module('mavenApp.controllers');
+
+app.controller('SettingsController', ['$scope', 'Setting','Gateway', function($scope, Setting,Gateway) {
+
+		Setting.get().then(function(result){
+			$scope.setting = result.data;
+		});
+		
+		Gateway.get().then(function(result){
+			$scope.gateways = result.data;
+			console.log(result.data);
+		});
+
+		$scope.saveSettings = function() {
+			Setting.save($scope.setting);
+		};
+	}]
+);
