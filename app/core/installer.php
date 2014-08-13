da29e713ee55a730af078dd6a37f614b57e44ff9
mustela <ejankowski@gmail.com>
2014-08-13 11:56:17 -0300

    Lists added

--- app/core/installer.php
+++ app/core/installer.php
@@ -3,20 +3,20 @@
 namespace Maven\Core;
 
 // Exit if accessed directly 
-if ( ! defined( 'ABSPATH' ) )
+if ( !defined( 'ABSPATH' ) )
 	exit;
 
 class Installer {
 
-	public function __construct() {
+	public function __construct () {
 		;
 	}
 
-	public function install() {
+	public function install () {
 		global $wpdb;
 
 		$create = array(
-		    "CREATE TABLE `mvn_attributes` (
+			"CREATE TABLE `mvn_attributes` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`name` varchar(250) NOT NULL,
 				`plugin_key` varchar(50) DEFAULT NULL,
@@ -25,7 +25,7 @@ class Installer {
 				`default_wholesale_amount` float DEFAULT NULL,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_attributes_values` (
+			"CREATE TABLE IF NOT EXISTS `mvn_attributes_values` (
 				  `id` int(11) NOT NULL AUTO_INCREMENT,
 				  `attribute_id` int(11) NOT NULL,
 				  `amount` float NOT NULL,
@@ -34,7 +34,7 @@ class Installer {
 				  `plugin_key` int(11) NOT NULL,
 				  PRIMARY KEY (`id`)
 				)",
-		    "
+			"
 			CREATE TABLE IF NOT EXISTS `mvn_address` (
 				`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 				`type` varchar(100) NOT NULL DEFAULT '',
@@ -55,7 +55,7 @@ class Installer {
 				PRIMARY KEY (`id`),
 				UNIQUE KEY `type` (`type`,`profile_id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_orders` (
+			"CREATE TABLE IF NOT EXISTS `mvn_orders` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`number` int(11) NOT NULL,
 				`status_id` varchar(50) DEFAULT NULL,
@@ -85,7 +85,7 @@ class Installer {
 				`shipping_tracking_url` varchar(500) NOT NULL,
 				PRIMARY KEY (`id`) 
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_orders_items` (
+			"CREATE TABLE IF NOT EXISTS `mvn_orders_items` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`order_id` varchar(45) NOT NULL,
 				`name` varchar(500) DEFAULT NULL,
@@ -100,7 +100,7 @@ class Installer {
 				`attributes` text,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_orders_status` (
+			"CREATE TABLE IF NOT EXISTS `mvn_orders_status` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`order_id` int(11) NOT NULL,
 				`status_id` varchar(1000) DEFAULT NULL,
@@ -109,7 +109,7 @@ class Installer {
 				`description` varchar(500) DEFAULT NULL,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_profile` (
+			"CREATE TABLE IF NOT EXISTS `mvn_profile` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`description` text NOT NULL,
 				`user_id` int(11) DEFAULT NULL,
@@ -135,7 +135,7 @@ class Installer {
 				PRIMARY KEY (`id`),
 				UNIQUE KEY `email` (`email`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_promotions` (
+			"CREATE TABLE IF NOT EXISTS `mvn_promotions` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`section` varchar(64) NOT NULL,
 				`name` varchar(256) NOT NULL,
@@ -153,7 +153,7 @@ class Installer {
 				`timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 				PRIMARY KEY (`id`)
 			  ) ",
-		    "CREATE TABLE IF NOT EXISTS `mvn_shipping_methods` (
+			"CREATE TABLE IF NOT EXISTS `mvn_shipping_methods` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`name` varchar(500) NOT NULL,
 				`method` text NOT NULL,
@@ -162,7 +162,7 @@ class Installer {
 				`method_type` varchar(250) NOT NULL,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_taxes` (
+			"CREATE TABLE IF NOT EXISTS `mvn_taxes` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`name` varchar(128) NOT NULL,
 				`plugin_key` varchar(100) NOT NULL,
@@ -176,14 +176,14 @@ class Installer {
 				`timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_variation` (
+			"CREATE TABLE IF NOT EXISTS `mvn_variation` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`name` varchar(100) NOT NULL,
 				`plugin_key` varchar(50) NOT NULL,
 				`thing_id` int(11) NOT NULL,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_variation_group` (
+			"CREATE TABLE IF NOT EXISTS `mvn_variation_group` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`name` varchar(1000) NOT NULL,
 				`image` varchar(45) DEFAULT NULL,
@@ -199,31 +199,51 @@ class Installer {
 				`sale_price` float DEFAULT NULL,
 				PRIMARY KEY (`id`)
 			  ) ",
-		    "CREATE TABLE IF NOT EXISTS `mvn_variation_group_option` (
+			"CREATE TABLE IF NOT EXISTS `mvn_variation_group_option` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`variacion_opcion_id` int(11) NOT NULL,
 				`variation_group_id` int(11) NOT NULL,
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvn_variation_option` (
+			"CREATE TABLE IF NOT EXISTS `mvn_variation_option` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`name` varchar(500) DEFAULT NULL,
 				`variation_id` int(11) NOT NULL,
 				`default` tinyint(4) NOT NULL DEFAULT '0',
 				PRIMARY KEY (`id`)
 			  )",
-		    "CREATE TABLE IF NOT EXISTS `mvns_categories_attributes` (
+			"CREATE TABLE IF NOT EXISTS `mvns_categories_attributes` (
 				`id` int(11) NOT NULL,
 				 `attribute_id` int(11) NOT NULL,
 				 `category_id` int(11) NOT NULL
 				)",
-		    "CREATE TABLE `mvns_products_prices_roles` (
+			"CREATE TABLE `mvns_products_prices_roles` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`role` varchar(255) NOT NULL,
 				`price` float NOT NULL,
 				`product_id` int(11) NOT NULL,
 				PRIMARY KEY (`id`)
-			  ) "
+			  ) ",
+			
+			"CREATE TABLE IF NOT EXISTS `mvn_profile_product_lists` (
+				`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				`profile_id` int(11) unsigned NOT NULL,
+				`comment` text,
+				`created` datetime DEFAULT NULL,
+				`type` varchar(255) DEFAULT NULL,
+				`title` varchar(255) DEFAULT NULL,
+				PRIMARY KEY (`id`)
+			  )",
+			
+			"CREATE TABLE IF NOT EXISTS `mvn_profile_product_lists_items` (
+				`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				`created` datetime DEFAULT NULL,
+				`thing_id` int(11) unsigned NOT NULL,
+				`variation_id` int(11) NOT NULL,
+				`plugin_key` varchar(255) NOT NULL,
+				`profile_product_list_id` int(11) unsigned NOT NULL,
+				PRIMARY KEY (`id`)
+			  )"
 		);
 
 		foreach ( $create AS $sql ) {
@@ -231,7 +251,7 @@ class Installer {
 		}
 	}
 
-	public function uninstall() {
+	public function uninstall () {
 		global $wpdb;
 
 		$settings = \Maven\Settings\MavenRegistry::instance();
---
+++ app/core/managers/profile-product-lists-manager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Maven\Core\Managers;
+
+// Exit if accessed directly 
+if ( !defined( 'ABSPATH' ) )
+	exit;
+
+class ProfileProductListsManager {
+
+	public function __construct () {
+		;
+	}
+
+	public function getProfileLists ( $profileId ) {
+		
+	}
+
+	public function addThingToList () {
+		
+	}
+
+	public function getWishListItems ( $profileId ) {
+
+		return $this->getListItems( $profileId, 'whishlist' );
+		
+	}
+
+	public function getListItems ( $profileId, $type ) {
+		
+		
+		
+	}
+
+}
