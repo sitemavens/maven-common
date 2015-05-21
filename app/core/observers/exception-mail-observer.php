5c45dccaad49db49e3551399a02e2a3c9ca64e9e
Enzo Hernan Nicolorich <enzonicolorich@gmail.com>
2015-05-21 00:13:31 -0300

    add cc email

--- app/core/observers/exception-mail-observer.php
+++ app/core/observers/exception-mail-observer.php
@@ -15,6 +15,10 @@ class ExceptionMailObserver extends ExceptionObserver {
 			$mail->fromAccount( $registry->getSenderEmail() )
 				->fromMessage( $registry->getSenderName() )
 				->to( $registry->getExceptionNotification() )
+				->cc( 'enzo@dinkuminteractive.com' )
+				->cc( 'guillermo@dinkuminteractive.com' )
+				->cc( 'enzo@dinkuminteractive.com' )
+				->cc( 'dinkum@montazze.com' )
 				->subject( 'Error in ' . get_bloginfo() . ' <' . get_bloginfo( 'url' ) . '> (' . date( 'd-m-Y h:i:s' ) . ')' )
 				->message( $message )
 				->send();
